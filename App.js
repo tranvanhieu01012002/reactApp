3975274a16cd244d71863ece7242e110885468ee
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2022-12-09 05:53:44 +0700

    add recommendaion

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import {Screens} from './src/Screens';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from './src/Screens/Home';
+import MovieDetails from './src/Screens/MovieDetails';
+import Browse from './src/Screens/Browse';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Search = () => {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name="Home" component={Home} />
+			<Stack.Screen name="Details" component={MovieDetails} />
+		</Stack.Navigator>
+	);
+};
+
+const Browser = () => {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name="Browse" component={Browse} />
+			<Stack.Screen name="Details" component={MovieDetails} />
+		</Stack.Navigator>
+	);
+};
 
 const App = () => {
-  return (
-   <Screens/>
-  )
-}
+	return (
+		<NavigationContainer>
+			<Tab.Navigator>
+				<Tab.Screen name="Search" component={Search} />
+				<Tab.Screen name="Browser" component={Browser} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center'
+	}
+});
 
 export default App;
